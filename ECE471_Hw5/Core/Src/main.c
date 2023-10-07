b899feeaa57e02aa3d639b4ec8ee6ed8c2785785
Nathan Turner <ndturner@mail.bradley.edu>
2023-10-07 11:59:20 -0500

    added adc functionality

--- ECE471_Hw5/Core/Src/main.c
+++ ECE471_Hw5/Core/Src/main.c
@@ -20,10 +20,10 @@
 #include "main.h"
 #include "string.h"
 #include "cmsis_os.h"
-#include <stdbool.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdbool.h>
 
 /* USER CODE END Includes */
 
@@ -62,6 +62,8 @@ ETH_DMADescTypeDef DMATxDscrTab[ETH_TX_DESC_CNT] __attribute__((section(".TxDecr
 
 ETH_TxPacketConfig TxConfig;
 
+ADC_HandleTypeDef hadc1;
+
 ETH_HandleTypeDef heth;
 
 UART_HandleTypeDef huart3;
@@ -86,6 +88,7 @@ static void MX_GPIO_Init(void);
 static void MX_ETH_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
+static void MX_ADC1_Init(void);
 void StartDefaultTask(void const * argument);
 void alive_init(void const * argument);
 void button_init(void const * argument);
@@ -93,10 +96,8 @@ void light_init(void const * argument);
 void analog_init(void const * argument);
 void control_init(void const * argument);
 
-
-
 /* USER CODE BEGIN PFP */
-
+static bool Toggle =false;
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -135,6 +136,7 @@ int main(void)
   MX_ETH_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -255,6 +257,58 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -483,8 +537,6 @@ void alive_init(void const * argument)
 * @retval None
 */
 /* USER CODE END Header_button_init */
-
-static bool Toggle = false;
 void button_init(void const * argument)
 {
   /* USER CODE BEGIN button_init */
@@ -522,11 +574,9 @@ void light_init(void const * argument)
 
 	if(Toggle){
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET); //green
-
 	}
 	else{
 		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET); //green
-
 	}
 
     osDelay(1);
@@ -547,6 +597,30 @@ void analog_init(void const * argument)
   /* Infinite loop */
   for(;;)
   {
+	  	  HAL_ADC_Start(&hadc1);
+
+	  	  // Wait for the ADC conversion to complete (blocking)
+	  	  HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+
+	  	  // Read the raw ADC value
+	  	  uint16_t raw = HAL_ADC_GetValue(&hadc1);
+	  	  osStatus status = osMessagePut(ThrottleHandle, raw, osWaitForever);
+
+	  	  // 12-bit ADC with VREF = 3.0V
+	  	 // uint8_t transmit = 255*raw/4095;
+
+	  	 // HAL_UART_Transmit(&huart3, (uint8_t*)&transmit, 1, HAL_MAX_DELAY);
+
+	  	  // Print the voltage value
+
+
+	  	// sprintf(msg, "%.2fV\r\n", voltage);
+
+	  	  // transmit on uart3 for
+	  	  //HAL_UART_Transmit(&huart3, *voltage, 1, 100);
+
+
+
     osDelay(1);
   }
   /* USER CODE END analog_init */
